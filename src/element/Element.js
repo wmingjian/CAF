bcf4f1b6ae22e74f5b3ebfc999c4bfac68673cd4
wmingjian <wmingjian@ubuntu.(none)>
2010-12-18 05:28:22 -0800

    Element->element(add element.js)

---
+++ src/element/Element.js
@@ -0,0 +1,14 @@
+CAF.Element = {
+	getStyleProperty: function(name){
+	},
+	setStyleProperty: function(name, value){
+	},
+	getVisible: function(){
+	},
+	setVisible: function(visible){
+	},
+	getOpacity: function(){
+	},
+	setOpacity: function(opacity){
+	}
+};
