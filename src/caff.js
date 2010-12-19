6a08a843cc8594e403a4f609cc0485ed48ae22a8
RobinQu <robinqu@gmail.com>
2010-12-19 17:06:10 +0800

    adding some utils to CAF.js

--- src/caff.js
+++ src/caff.js
@@ -20,7 +20,7 @@ CAF.Model
 
 CAF.UI.Control
 CAF.UI.Layout
-CAF.UI.Text
+CAF.UI.TCAF
 
 CAF.Service
 
@@ -30,49 +30,8 @@ CAF.Service
 CAF
 */
 
-
-window.CAF = {
+var CAF = {
 	version: 1
-};
-
-/**
- * 类继承语法封装
- *
- * @param superClass {Class} 父类的名字
- * @param classImp {Object|Function} 当前类的实现
- * @return {Class} 当前类
- */
-CAF.extend = function(superClass, classImp){
-	var clazz = function(){
-		if(this.init == "function"){
-			this.init.apply(this, arguments);
-		}
-	};
-	clazz.prototype = new superClass();
-	var type = typeof classImp;
-	if(type == "object"){
-		for(var k in classImp){
-			clazz[k] = classImp[k];
-		}
-	}else if(type == "function"){
-		classImp.apply(clazz);	
-	}else{
-		throw "param 'classImp' error!";
-	}
-	return clazz;
-};
-
-/**
- * 动态绑定函数对象中this的值
- *
- * @param func {Function}
- * @param obj {Object}
- * @return {Function}
- */
-CAF.bind = function(func, obj){
-	return function(){
-		return func.apply(obj, arguments);
-	};
 };
 
 /**
@@ -82,15 +41,16 @@ CAF.bind = function(func, obj){
  * @param flag {Boolean}
  */
 CAF.cloneTo = function(dest, src, flag){
+	var k;
 	if(dest && src && typeof src == 'object'){
 		if(flag){
-		    for(var k in src){
+		    for(k in src){
 		        if(!(k in dest)){
 		            dest[k] = src[k];
 		        }
 		    }
 		}else{
-		    for(var k in src){
+		    for(k in src){
 		        dest[k] = src[k];
 		    }
 		}
@@ -98,3 +58,161 @@ CAF.cloneTo = function(dest, src, flag){
     return dest;
 };
 
+CAF.cloneTo(CAF, {
+	/**
+	 * 动态绑定函数对象中this的值
+	 *
+	 * @param func {Function}
+	 * @param obj {Object}
+	 * @return {Function}
+	 */
+	bind : function(func, obj){
+		return function(){
+			return func.apply(obj, arguments);
+		};
+	},
+	/**
+	 * 类继承语法封装
+	 *
+	 * @param SuperClass {Class} 父类的名字
+	 * @param classImp {Object|Function} 当前类的实现
+	 * @return {Class} 当前类
+	 */
+	CAFend : function(SuperClass, classImp){
+		var clazz = function(){
+			if(this.init == "function"){
+				this.init.apply(this, arguments);
+			}
+		};
+		clazz.prototype = new SuperClass();
+		var type = typeof classImp;
+		if(type == "object"){
+			for(var k in classImp){
+				clazz[k] = classImp[k];
+			}
+		}else if(type == "function"){
+			classImp.apply(clazz);	
+		}else{
+			throw "param 'classImp' error!";
+		}
+		return clazz;
+	},
+	
+	/**
+     * <p>Returns true if the passed value is empty.</p>
+     * <p>The value is deemed to be empty if it is<div class="mdetail-params"><ul>
+     * <li>null</li>
+     * <li>undefined</li>
+     * <li>an empty array</li>
+     * <li>a zero length string (Unless the <tt>allowBlank</tt> parameter is <tt>true</tt>)</li>
+     * </ul></div>
+     * @param {Mixed} value The value to test
+     * @param {Boolean} allowBlank (optional) true to allow empty strings (defaults to false)
+     * @return {Boolean}
+     */
+    isEmpty : function(value, allowBlank) {
+        var isNull       = value === null,
+            emptyArray   = (CAF.isArray(value) && !value.length),
+            blankAllowed = !allowBlank ? value === '' : false;
+
+        return isNull || emptyArray || blankAllowed;
+    },
+
+    /**
+     * Returns true if the passed value is a JavaScript array, otherwise false.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isArray : function(v) {
+        return Object.prototype.toString.apply(v) === '[object Array]';
+    },
+
+    /**
+     * Returns true if the passed object is a JavaScript date object, otherwise false.
+     * @param {Object} object The object to test
+     * @return {Boolean}
+     */
+    isDate : function(v) {
+        return Object.prototype.toString.apply(v) === '[object Date]';
+    },
+
+    /**
+     * Returns true if the passed value is a JavaScript Object, otherwise false.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isObject : function(v) {
+        return !!v && !v.tagName && Object.prototype.toString.call(v) === '[object Object]';
+    },
+
+    /**
+     * Returns true if the passed value is a JavaScript 'primitive', a string, number or boolean.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isPrimitive : function(v) {
+        return CAF.isString(v) || CAF.isNumber(v) || CAF.isBoolean(v);
+    },
+
+    /**
+     * Returns true if the passed value is a JavaScript Function, otherwise false.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isFunction : function(v) {
+        return Object.prototype.toString.apply(v) === '[object Function]';
+    },
+
+    /**
+     * Returns true if the passed value is a number. Returns false for non-finite numbers.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isNumber : function(v) {
+        return Object.prototype.toString.apply(v) === '[object Number]' && isFinite(v);
+    },
+
+    /**
+     * Returns true if the passed value is a string.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isString : function(v) {
+        return typeof v === 'string';
+    },
+
+    /**util
+     * Returns true if the passed value is a boolean.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isBoolean : function(v) {
+        return Object.prototype.toString.apply(v) === '[object Boolean]';
+    },
+
+    /**
+     * Returns true if the passed value is an HTMLElement
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isElement : function(v) {
+        return v ? !!v.tagName : false;
+    },
+
+    /**
+     * Returns true if the passed value is not undefined.
+     * @param {Mixed} value The value to test
+     * @return {Boolean}
+     */
+    isDefined : function(v){
+        return typeof v !== 'undefined';
+    },
+
+	_nextGUID : 0,
+	guidFor : function(obj) {
+		// special cases where we don't want to add a key to object
+	    if (obj === undefined) return "(undefined)";
+	    if (obj === null) return '(null)';
+		return this._nextGUID++;
+	}
+});
--- src/util/Observable.js
+++ src/util/Observable.js
@@ -3,3 +3,24 @@
  * @author RobinQu
  * @version 0.1
  */
+
+CAF.Obseravable = {
+    isObservable: true,
+    init: function(config) {
+        var me = this;
+
+        CAF.cloneTo(me, config);
+        if (me.listeners) {
+            me.on(me.listeners);
+            delete me.listeners;
+        }
+        me.events = me.events || {};
+    },
+	addListener: function(ename, fn, scope) {
+		var events = this.events,
+			ev = events[ename] = events[ename] || [];
+		scope = scope || this;
+		
+		
+	},
+};
